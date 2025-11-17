8e3336962440d823f421d37ef8b3c3f2c7eeaecf
WakilIsrakAbir <wakilisrakabir@gmail.com>
2025-11-17 19:45:43 +0600

    Update list_string.py

--- Class 2/list_string.py
+++ Class 2/list_string.py
@@ -6,4 +6,119 @@ while True:
     n=int(n)
     running_total=running_total+n
     print("Running Total:",running_total)
-  
+
+
+s="abc"
+s1=s.lower()
+s2=s.upper()
+print(s1,s2)
+s="this is a title"
+s=s.title()
+print(s)
+print(len(s))
+print(s[len(s)-1])
+print(s[-1]
+
+
+n=5
+print(id(n))
+n=n+15
+print(id(n))
+
+
+item=[1,2,3,"abc",10.5]
+x="abc"
+print(x in item)
+print(item[0],item[1])
+print(item[-1],item[-2])
+n=len(item)
+print(n)
+print(type(item))
+
+for n in item:
+    print(n)
+
+country="Bangla"
+for c in country:
+    print(c)
+    
+li=[1,2,3]
+li[0]=4
+print(li)
+
+
+city="Dhaka"
+li=list(city)
+print(li)
+li[0]='d'
+print(li)
+
+list=['d', 'h', 'a', 'k', 'a']
+list[0]="D"
+s=''.join(list)
+print(s)
+
+
+n1="5"
+n2="5"
+print(n1+n2)
+
+
+#codingBat :string1    problem solve
+#make_out_word
+def make_out_word(out, word):
+  s=out[0:2]+word+out[2:4]
+  return s
+
+#first_two
+def first_two(str):
+  if len(str) < 2:
+    return str
+  else:
+    return str[0:2]
+
+#without_end
+def without_end(str):
+  return str[1:-1]
+    
+#non_start
+def non_start(a,b):
+  return a[1:]+b[1:]
+
+
+#list-1      problem solve
+#first_last6
+def first_last6(nums):
+  if nums[0]== 6 :
+    return True
+  if nums[-1]==6  :
+    return True
+  else:
+    return False
+#0r
+def first_last6(nums):
+  if nums[0]== 6 or nums[-1]==6:
+    return True
+  else:
+    return False
+
+#make_pi
+def make_pi():
+  pi=[]
+  pi.append(3)
+  pi.append(1)
+  pi.append(4)
+  return pi
+
+#sym3
+def sum3(nums):
+  return nums[0]+nums[1]+nums[2]
+#or
+    def sum3(nums):
+  return sum(nums)
+
+#reverse3
+def reverse3(nums):
+  nums[0],nums[2]=nums[2],nums[0]
+  return nums
+
